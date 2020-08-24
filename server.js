bc129a900eb507bd6ffdef534e58a32dcf45b062
nikoganev <niko.venag@gmail.com>
2020-08-24 04:26:20 +0200

    changed server client path

--- server.js
+++ server.js
@@ -46,12 +46,10 @@ app.use("/requests", requests);
 
 
 
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+const root = require('path').join(__dirname, 'client', 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+})
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
